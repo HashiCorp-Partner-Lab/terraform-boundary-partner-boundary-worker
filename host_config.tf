fc9c1f13f785fce067ec50ba508be24c51100402
dannyjknights <dannyjknights@gmail.com>
2024-01-10 15:19:49 +0000

    scope id stuff

--- host_config.tf
+++ host_config.tf
@@ -31,10 +31,19 @@
 #   ]
 # }
 
+data "boundary_scope" "org" {
+  name     = "test-org"
+  scope_id = "global"
+}
+
+data "boundary_scope" "project" {
+  name     = "test-project"
+  scope_id = data.boundary_scope.org.id
+}
 
 
 resource "boundary_host_catalog_static" "smw_static_host_catalog" {
-  scope_id    = var.project_id
+  scope_id    = data.boundary_scope.project.id
   description = "Self Managed Worker Host Catalogue"
 }
 
