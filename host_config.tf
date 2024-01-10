dbe7530838eee2f1455b8c3a81f66a47b875b942
dannyjknights <dannyjknights@gmail.com>
2024-01-10 16:51:04 +0000

    Added example target

--- host_config.tf
+++ host_config.tf
@@ -1,35 +1,35 @@
-# resource "boundary_host_catalog_static" "example_host_catalog" {
-#   scope_id    = var.project_id
-#   description = "Example Host Catalogue"
-# }
+resource "boundary_host_catalog_static" "example_host_catalog" {
+  scope_id    = var.project_id
+  description = "Example Host Catalogue"
+}
 
-# resource "boundary_host_set_static" "example_static_host_set" {
-#   host_catalog_id = boundary_host_catalog_static.example_host_catalog.id
-#   name            = "example-static-host-set"
-#   description     = "Example Static Host Set for AWS Linux Machine"
-#   host_ids        = [boundary_host_static.example_static_host.id]
-# }
+resource "boundary_host_set_static" "example_static_host_set" {
+  host_catalog_id = boundary_host_catalog_static.example_host_catalog.id
+  name            = "example-static-host-set"
+  description     = "Example Static Host Set for AWS Linux Machine"
+  host_ids        = [boundary_host_static.example_static_host.id]
+}
 
-# resource "boundary_host_static" "example_static_host" {
-#   host_catalog_id = boundary_host_catalog_static.example_host_catalog.id
-#   address         = aws_instance.boundary_public_target.public_ip
-#   name            = "example-static-host"
-#   description     = "Example Static Host for AWS Linux EC2"
-#   depends_on      = [aws_instance.boundary_public_target]
-# }
+resource "boundary_host_static" "example_static_host" {
+  host_catalog_id = boundary_host_catalog_static.example_host_catalog.id
+  address         = aws_instance.boundary_public_target.public_ip
+  name            = "example-static-host"
+  description     = "Example Static Host for AWS Linux EC2"
+  depends_on      = [aws_instance.boundary_public_target]
+}
 
-# resource "boundary_target" "aws_linux_public" {
-#   type                     = "tcp"
-#   name                     = "aws-public-linux"
-#   description              = "AWS Linux Public Target"
-#   egress_worker_filter     = " \"sm-ingress-upstream-worker1\" in \"/tags/type\" "
-#   scope_id                 = var.project_id
-#   session_connection_limit = -1
-#   default_port             = 22
-#   host_source_ids = [
-#     boundary_host_set_static.example_static_host_set.id
-#   ]
-# }
+resource "boundary_target" "aws_linux_public" {
+  type                     = "tcp"
+  name                     = "aws-public-linux"
+  description              = "AWS Linux Public Target"
+  egress_worker_filter     = " \"sm-ingress-upstream-worker1\" in \"/tags/type\" "
+  scope_id                 = var.project_id
+  session_connection_limit = -1
+  default_port             = 22
+  host_source_ids = [
+    boundary_host_set_static.example_static_host_set.id
+  ]
+}
 
 data "boundary_scope" "org" {
   name     = "test-org"
--- security-groups.tf
+++ security-groups.tf
@@ -1,25 +1,25 @@
-# resource "aws_security_group" "static_target_sg" {
-#   name        = "SG for Boundary Public Static Target"
-#   description = "SG for Boundary Public Static Target"
-#   vpc_id      = aws_vpc.boundary_ingress_worker_vpc.id
+resource "aws_security_group" "static_target_sg" {
+  name        = "SG for Boundary Public Static Target"
+  description = "SG for Boundary Public Static Target"
+  vpc_id      = aws_vpc.boundary_ingress_worker_vpc.id
 
-#   ingress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = -1
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = [aws_instance.boundary_ingress_worker.public_ip, aws_instance.boundary_ingress_worker.private_ip]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#   tags = {
-#     Name = "allow_all"
-#   }
-# }
+  tags = {
+    Name = "allow_all_from_worker"
+  }
+}
 
 resource "aws_security_group" "boundary_ingress_worker_ssh" {
   name        = "boundary_ingress_worker_allow_ssh_9202"
