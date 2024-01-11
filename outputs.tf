d9927a4492a66d4683224a5017684a37c55d0e1d
dannyjknights <dannyjknights@gmail.com>
2024-01-11 11:21:43 +0000

    some tidying up

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,3 @@
-output "scope_id_output" {
-  value = var.project_id
-}
-
-output "project_id_output_new" {
+output "project_id_output" {
   value = data.boundary_scope.project.id
-}
-
-output "org_id_output" {
-  value = data.boundary_scope.org
-}
-
-output "example_target_public_ip" {
-  value = aws_instance.boundary_ingress_worker.public_ip
 }
